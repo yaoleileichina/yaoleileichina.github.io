623e3110600299e0bf31d869e5e275bf2296011e
yaoleileichina <46495433+yaoleileichina@users.noreply.github.com>
2019-01-09 01:06:39 +0800

    Add files via upload

---
+++ first_test/src/cn/yl/test/First.java
@@ -0,0 +1,14 @@
+package cn.yl.test;
+
+public class First {
+	
+	
+	public  static   int count  = 10 ;
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("sdsds");
+	}
+
+}
